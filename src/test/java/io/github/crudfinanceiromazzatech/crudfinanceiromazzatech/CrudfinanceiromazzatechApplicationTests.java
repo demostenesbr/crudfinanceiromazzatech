804da2133fd165c04cfbe591a505c8b57d54c949
Demóstenes Muniz Brito <demostenes@demostenes.com.br>
2025-01-25 18:15:13 -0300

    Feat: first commit with Spring Boot Java default

---
+++ src/test/java/io/github/crudfinanceiromazzatech/crudfinanceiromazzatech/CrudfinanceiromazzatechApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.crudfinanceiromazzatech.crudfinanceiromazzatech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudfinanceiromazzatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
