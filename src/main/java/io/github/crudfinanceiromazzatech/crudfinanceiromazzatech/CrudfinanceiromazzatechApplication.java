a2b301a3b56a048805b5d5865891757507b292ce
Demóstenes Muniz Brito <demostenes@demostenes.com.br>
2025-01-26 18:22:00 -0300

    Fix: update CaseCamel of project

--- src/main/java/io/github/crudfinanceiromazzatech/crudfinanceiromazzatech/CrudfinanceiromazzatechApplication.java
+++ src/main/java/io/github/crudfinanceiromazzatech/crudfinanceiromazzatech/CrudfinanceiromazzatechApplication.java
@@ -1,13 +1,20 @@
-package io.github.crudfinanceiromazzatech.crudfinanceiromazzatech;
+package io.github.crudfinanceiromazzatech.CrudFinanceiroMazzatech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-public class CrudfinanceiromazzatechApplication {
+@RestController
+public class CrudFinanceiroMazzatechApplication {
+
+	@GetMapping("/hello-world")
+	public String HelloWorld() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(CrudfinanceiromazzatechApplication.class, args);
+		SpringApplication.run(CrudFinanceiroMazzatechApplication.class, args);
 	}
 
 }
--- src/test/java/io/github/crudfinanceiromazzatech/crudfinanceiromazzatech/CrudfinanceiromazzatechApplicationTests.java
+++ src/test/java/io/github/crudfinanceiromazzatech/crudfinanceiromazzatech/CrudfinanceiromazzatechApplicationTests.java
@@ -1,10 +1,10 @@
-package io.github.crudfinanceiromazzatech.crudfinanceiromazzatech;
+package io.github.crudfinanceiromazzatech.CrudFinanceiroMazzatech;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CrudfinanceiromazzatechApplicationTests {
+class CrudFinanceiroMazzatechApplicationTests {
 
 	@Test
 	void contextLoads() {
